b20ff65c4bd10d25b84d2b32cefdc8dbc2615d53
Lindsey Sherrill <lindsey350@aol.com>
2022-05-06 12:37:01 -0400

    Added user prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
-// Assignment code here
 
+//GIVEN I need a new, secure password
+//WHEN I click the button to generate a password
+//THEN I am presented with a series of prompts for password criteria
+//WHEN prompted for password criteria
+//THEN I select which criteria to include in the password
+//WHEN prompted for the length of the password
+//THEN I choose a length of at least 8 characters and no more than 128 characters
+//WHEN asked for character types to include in the password
+//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+//WHEN I answer each prompt
+//THEN my input should be validated and at least one character type should be selected
+//WHEN all prompts are answered
+//THEN a password is generated that matches the selected criteria
+//WHEN the password is generated
+//THEN the password is either displayed in an alert or written to the page
+
+// Assignment code here
+function promptUser() {
+  var length = prompt("What is the length of your passwrord?", 8)
+  if(length < 8 || length > 128) {
+    alert("Please pick a password length between 8 and 128")
+    return null
+  }
+  var lowercase = confirm("Would you like lowercase sharacters in your password?");
+  var uppercase = confirm("Would you like uppercase characters in your password?");
+  var numbers = confirm("Would you like numbers in your password?");
+  var special = confirm("Would you like special characters in you password?");
+  var userResponse = {
+    length, 
+    lowercase, 
+    uppercase, 
+    numbers, 
+    special
+  }
+  return userResponse;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
